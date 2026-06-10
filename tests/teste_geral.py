fec9bb37fc2276e5899621cf14d1b9c8988afed0
danielc-silva <danielsilva.pf021@academico.ifsul.edu.br>
2026-06-09 22:24:26 -0300

    teste das Factorys

--- tests/teste_geral.py
+++ tests/teste_geral.py
@@ -11,6 +11,9 @@ from dao.pessoa_dao import PessoaDAO
 from dao.animal_dao import AnimalDAO
 from dao.manejos_dao import ManejosDAO 
 
+from model.pessoa_factory import PessoaFactory
+from model.animal_factory import AnimalFactory
+
 def executar_teste():
     print("=== INICIANDO TESTE COMPLETO DO SISTEMA ===\n")
 
@@ -36,9 +39,33 @@ def executar_teste():
         print(f"Aviso/Erro Pessoa: {e}")
 
 
+    print("--- Testando PessoaFactory ---")
+    try:
+        prop = PessoaFactory.criar_pessoa("Proprietario",
+            cpf="81238070078", 
+            nome="João da Silva", 
+            telefone="54999999999", 
+            inscricao_estadual="123456789", 
+            nome_fazenda="Fazenda Esperança"
+        )
+        sucesso, msg = pessoa_dao.salvar(prop)
+        print(f"Resultado: {msg}")
+    except Exception as e:
+        print(f"Aviso/Erro Pessoa: {e}")
+
+
     print("\n--- Testando AnimalDAO ---")
     try:
         vaca = Femea(brinco=brinco_teste, raca=Raca.ANGUS, data_nascimento="10-01-2022")
+        vaca.estado_reprodutivo = "Vazia" 
+        sucesso, msg = animal_dao.salvar(vaca)
+        print(f"Resultado: {msg}")
+    except Exception as e:
+        print(f"Aviso/Erro Animal: {e}")
+
+    print("\n--- Testando AnimalFactory ---")
+    try:
+        vaca = AnimalFactory.criar_animal("Macho", brinco= 987, raca=Raca.ANGUS, data_nascimento="10-01-2022")
         vaca.estado_reprodutivo = "Vazia" 
         sucesso, msg = animal_dao.salvar(vaca)
         print(f"Resultado: {msg}")
